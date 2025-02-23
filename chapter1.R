e7290fcf5efad1667e0047c5a29f52e1e9541732
nissou62 <125081076+nissou62@users.noreply.github.com>
2025-02-23 15:52:43 -0600

    Add files via upload

---
+++ chapter1.R
@@ -0,0 +1,99 @@
+1+1
+100:130
+5 - 1
+3 / 5
+2*3
+2 * 3
+4 -1
+6 / (4 - 1)
+10 +2
+12 * 3
+36 -6
+30 / 3
+1:6
+a <- 1
+a
+a + 2
+die <- 1:6
+die
+name <- 1
+name <- 0
+name <+ 1
+name <- 1
+name <- 0
+name + 1
+my_number <- 1
+my_number
+my_number <- 999
+my_number
+die - 1
+die/2
+die*die
+1:2
+1:4
+die
+die + 1:2
+die %*% die
+die %o% die
+round (3.1415)
+factorial(3)
+mean(1:6)
+mean(die)
+round(mean(die))
+sample(x = 1:4, size = 2)
+sample (x = die, size = 1)
+sample(x = die, size = 1)
+sample(x = die, size = 1)
+round(3.1415, digits = 2)
+args(round)
+args(round)
+round(3.1415, digits = 2)
+sample(size = 1, x = die)
+sample(size = 1, x = die)
+sample(die, size = 2)
+sample(die, size = 2, replace = TRUE)
+dice <- sample(die, size = 2, replace = TRUE)
+dice
+sum(dice)
+dice
+dice
+dice
+die <- 1:6
+dice <- sample(die, size = 2, replace = TRUE)
+sum(dice)
+roll()
+roll()
+roll2()
+my_function <- function(){}
+roll <- function() 
+die <- 1:6
+dice <- sample(die, size = 2, replace = TRUE)
+sum(dice)
+sum()
+roll
+roll <- function() {
+die <- 1:6
+dice <- sample(die, size = 2, replace = TRUE)
+sum(dice)}
+roll()
+roll()
+dice
+sqrt(1)
+sqrt(2)
+dice <- sample(die,size = 2, replace = TRUE)
+two <- 1+1
+a <- sqrt(2)
+roll2<-function(){}
+roll2()
+roll2 <- function (bones){
+dice <- sample(bones, size=2, replace = TRUE)
+sum(dice)}
+roll2(bones = 1:4)
+roll2(bones = 1:6)
+roll2(1:20)
+roll2 <- function(bones = 1:6){
+dice <- sample(bones, size = 2, replace = TRUE)
+sum(dice)}
+roll2()
+
+
